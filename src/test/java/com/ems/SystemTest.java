3995f9c30aa7f04506a4fa391d048f1f6c00c40e
Debananda Das <debananda.das@publicissapient.com>
2022-07-10 22:41:01 +0530

    cds

--- src/test/java/com/ems/SystemTest.java
+++ src/test/java/com/ems/SystemTest.java
@@ -10,28 +10,28 @@ import org.springframework.web.client.RestTemplate;
 
 public class SystemTest {
 
-    @Test
-    public void testCreateReadDelete() {
-
-        RestTemplate restTemplate = new RestTemplate();
-        String url = "http://localhost:8080/employees/create";
-        Employee employee=new Employee();
-        employee.setEmpName("Debananda");
-        employee.setEmpContact(78);
-        employee.setEmpEmail("deba@ps.com");
-        employee.setEmpAddress("Assam");
-
-
-        ResponseEntity<Employee> entity = restTemplate.postForEntity(url, employee, Employee.class);
-        url="http://localhost:8080/employees";
-        Employee[] employees = restTemplate.getForObject(url, Employee[].class);
-
-        Assertions.assertThat(employees).extracting(Employee::getEmpName).contains("Debananda");
-
-
-        restTemplate.delete(url + "/" + entity.getBody().getEmpID());
-        Assertions.assertThat(restTemplate.getForObject(url, Employee[].class)).isEmpty();
-
-    }
+//    @Test
+//    public void testCreateReadDelete() {
+//
+//        RestTemplate restTemplate = new RestTemplate();
+//        String url = "http://localhost:8080/employees/create";
+//        Employee employee=new Employee();
+//        employee.setEmpName("Debananda");
+//        employee.setEmpContact(78);
+//        employee.setEmpEmail("deba@ps.com");
+//        employee.setEmpAddress("Assam");
+//
+//
+//        ResponseEntity<Employee> entity = restTemplate.postForEntity(url, employee, Employee.class);
+//        url="http://localhost:8080/employees";
+//        Employee[] employees = restTemplate.getForObject(url, Employee[].class);
+//
+//        Assertions.assertThat(employees).extracting(Employee::getEmpName).contains("Debananda");
+//
+//
+//        restTemplate.delete(url + "/" + entity.getBody().getEmpID());
+//        Assertions.assertThat(restTemplate.getForObject(url, Employee[].class)).isEmpty();
+//
+//    }
 
 }
